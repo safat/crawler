e84b930239f90d029a13187208b0f48906fc666b
M Shakhawat Hossain safat <shakhawat.hossain@therapservices.net>
2018-03-25 11:11:18 +0600

    Implemented 1 depth web crawler in Golang

---
+++ crawler.go
@@ -0,0 +1,52 @@
+package crawler
+
+import (
+	"net/http"
+	"golang.org/x/net/html"
+	"regexp"
+)
+
+type Anchor struct {
+	Link string
+	Text string
+}
+
+func Crawl(url string) []Anchor {
+	urlPrefixRegex, _ := regexp.Compile("^(http|https|ftp).*")
+	resp, _ := http.Get(url)
+
+	htmlTagTokenizer := html.NewTokenizer(resp.Body)
+
+	anchorList := make([]Anchor, 0)
+Loop:
+	for {
+		htmlTag := htmlTagTokenizer.Next()
+
+		switch {
+		case htmlTag == html.ErrorToken:
+			break Loop
+		case htmlTag == html.StartTagToken:
+			htmlToken := htmlTagTokenizer.Token()
+
+			if htmlToken.Data == "a" {
+				anchor, ok := parseAnchorTag(htmlToken, urlPrefixRegex)
+
+				if ok {
+					anchorList = append(anchorList, anchor)
+				}
+			}
+		}
+	}
+
+	return anchorList
+}
+
+func parseAnchorTag(htmlToken html.Token, urlPrefixRegex *regexp.Regexp) (Anchor, bool) {
+	for _, attribute := range htmlToken.Attr {
+		if attribute.Key == "href" && urlPrefixRegex.MatchString(attribute.Val) {
+			return Anchor{attribute.Val, ""}, true
+		}
+	}
+
+	return Anchor{}, false
+}
